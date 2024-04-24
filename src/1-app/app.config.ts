16d0702d180bea105f6c2c05accb209dea0d2ad4
Alexander Bashenin <timebender00@gmail.com>
2024-04-24 16:23:25 +0300

    script for generate types and api service

--- src/1-app/app.config.ts
+++ src/1-app/app.config.ts
@@ -2,6 +2,8 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { ApiConfiguration } from '@/shared/api/generated';
+import { API } from '@/shared/lib';
 import { routes } from './app.routes';
 import { authInterceptor } from './auth.interceptor';
 
@@ -10,5 +12,6 @@ export const appConfig: ApplicationConfig = {
         provideRouter(routes),
         provideAnimationsAsync(),
         provideHttpClient(withInterceptors([authInterceptor])),
+        { provide: ApiConfiguration, useValue: { rootUrl: API } },
     ],
 };
---
+++ update-api.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+fs.unlinkSync('./src/6-shared/api/generated/services.ts');
+
+fs.writeFileSync(
+    './src/6-shared/api/generated/services/index.ts',
+    `
+export { ApiService } from './api.service';
+
+`,
+    function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('The file was saved!');
+    },
+);
+
+fs.writeFileSync(
+    './src/6-shared/api/generated/index.ts',
+    `
+export { ApiService } from './services';
+
+export * from './models';
+
+`,
+    function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('The file was saved!');
+    },
+);
